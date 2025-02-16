827b507ee0c1bc3e00232cfea5504dd383441a62
Taro112233 <thanatouchth@gmail.com>
2025-02-16 19:48:29 +0700

    Installation

---
+++ job-marshal/components/general/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export function Navbar() {
+    return (
+        <nav className="flex items-center justify-between py-5">
+            <Link href="/">
+            <h1>
+                Job<span>Marshal</span>
+            </h1>
+            </Link>
+
+        <a href=""></a>
+        </nav>
+
+    )
+}
