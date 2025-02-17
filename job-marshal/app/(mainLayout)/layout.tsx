ceb5c607c467b8a9e900854fcacf8a530c992778
Taro112233 <thanatouchth@gmail.com>
2025-02-17 23:19:19 +0700

    wtf

--- job-marshal/app/(mainLayout)/layout.tsx
+++ job-marshal/app/(mainLayout)/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 export default function MainLayout({children} : {children: ReactNode}) {
     return (
-        <div className="container mx-auto">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <Navbar />
             {children}
         </div>
